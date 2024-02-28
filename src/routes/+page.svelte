e69acb717bb3cfcdf429711108935aa8d45451e8
Daniel Ebert <daniel.ebert@axtesys.at>
2024-02-28 15:26:33 +0100

    move todo to subroute

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,2 @@
-<script lang="ts">
-  import TodoList from "$lib/components/TodoList.svelte"
-</script>
-
-<div>
-  <TodoList />
-</div>
-
-<style lang="postcss">
-  div {
-    @apply h-full;
-    @apply flex flex-col items-center;
-  }
-</style>
+<a href="/todo">TodoList example</a>
+<a href="/weather">Weather example</a>
---
+++ src/routes/todo/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import TodoList from "$lib/components/TodoList.svelte"
+</script>
+
+<div>
+  <TodoList />
+</div>
+
+<style lang="postcss">
+  div {
+    @apply h-full;
+    @apply flex flex-col items-center;
+  }
+</style>
